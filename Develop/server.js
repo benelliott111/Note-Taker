963717f77b88e290c5263b5699a3eab01ebfe2bc
Ben Elliott <benelliott111@gmail.com>
2021-03-07 16:21:58 -0500

    working add note

--- Develop/server.js
+++ Develop/server.js
@@ -13,10 +13,58 @@ app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+const dbFile = './app/db.json';
+
+let db = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+
+let noteList = [{id: "0000-0000-0000-0000", title: 'note1', text: 'note1 text'}];
 
 app.listen(PORT, () => {
-console.log(`Example app listening at http://localhost:${PORT}`)
+console.log(`Note taker app listening at http://localhost:${PORT}`)
 })
+
+app.get('/notes', (req, res, next) => {
+    res.sendFile(__dirname + `/public/notes.html`)
+})
+
+app.get('/api/notes', (req, res, next) => {
+    console.log(db)
+    res.send(db)
+})
+
+app.post("/api/notes", (req, res) => {
+    // store
+    //   .addNote(req.body)
+    //   .then((note) => res.json(note))
+    //   .catch((err) => res.status(500).json(err));
+  
+    let newNote = req.body;
+    let highestId = 1;
+    if (db.length) {
+      highestId = Math.max(...db.map((note) => note.id));
+      highestId++;
+    }
+  
+    newNote.id = highestId;
+    db.push(newNote);
+  
+    res.json(db);
+  
+    fs.writeFileSync("db/db.json", JSON.stringify(db));
+  });
+  
+  app.delete("/api/notes/:id", (req, res) => {
+    // store
+    //   .removeNote(req.params.id)
+    //   .then(() => res.json({ ok: true }))
+    //   .catch((err) => res.status(500).json(err));
+  
+    let deleteNote = req.params.id;
+    db = db.filter((note) => note.id != deleteNote);
+    res.end("Note has been deleted");
+  });
+  
+
+
+// let title = document.querySelector('#noteTitle').value
+// let text = document.querySelector('#noteText').value
